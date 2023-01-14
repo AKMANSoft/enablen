ec543ad8dbf6d3ec5c4e9d171afeb9c8eae450ec
Ali Hussnain <ali.hussnain112003@gmail.com>
2023-01-14 17:58:24 +0500

    Enablen

--- scripts/carousel.js
+++
@@ -1,5 +0,0 @@
-var emblaNode = document.querySelector('.index_home_sldier')
-var options = { loop: false }
-var plugins = [EmblaCarouselAutoplay()]
-
-var embla = EmblaCarousel(emblaNode, options, plugins)
--- scripts/index.js
+++ scripts/index.js
@@ -1,88 +1,84 @@
-window.addEventListener("load", () => {
-    const tabsList = document.querySelectorAll("#solutions_tab_item");
+
+
+const tabsList = document.querySelectorAll("#solutions_tab_item");
 
 
 
 
-    function setupHomeCarousel() {
-        const setupProgressBar = (emblaApi, progressNode) => {
-            const applyProgress = () => {
-                const progress = Math.max(0, Math.min(1, emblaApi.scrollProgress()))
-                progressNode.style.width = `${progress * 100}%`
-            }
-            const removeProgress = () => {
-                progressNode.style.width = '0%'
-            }
-            return {
-                applyProgress,
-                removeProgress,
-            }
+function setupHomeCarousel() {
+    const setupProgressBar = (emblaApi, progressNode) => {
+        const applyProgress = () => {
+            const progress = Math.max(0, Math.min(1, emblaApi.scrollProgress()))
+            progressNode.style.width = `${progress * 100}%`
         }
-
-        const OPTIONS = { loop: true }
-        const homeSliderNode = document.getElementById('home_sldier');
-        const progressNode = document.getElementById('home_sider_progress_bar');
-        const homeSliderPrevBtn = document.getElementById("home_slider_prev_btn");
-        const homeSliderNextBtn = document.getElementById("home_slider_next_btn");
-        const homeSliderCurSlideNum = document.getElementById("home_slider_cur_slide_num");
-
-        const homeSliderCarousel = EmblaCarousel(homeSliderNode, OPTIONS)
-        const { applyProgress, removeProgress } = setupProgressBar(
-            homeSliderCarousel,
-            progressNode,
-        )
-
-        homeSliderNextBtn.onclick = homeSliderCarousel.scrollNext
-        homeSliderPrevBtn.onclick = homeSliderCarousel.scrollPrev
-
-        const applyCurrentSlideNum = () => {
-            const _value = homeSliderCarousel.selectedScrollSnap() + 1
-            if (_value < 10) homeSliderCurSlideNum.innerText = "0" + _value
-            else homeSliderCurSlideNum.innerText = _value
-
+        const removeProgress = () => {
+            progressNode.style.width = '0%'
         }
-
-        homeSliderCarousel
-            .on('init', () => {
-                applyProgress();
-                applyCurrentSlideNum();
-            })
-            .on('reInit', applyProgress)
-            .on('scroll', applyProgress)
-            .on('destroy', removeProgress)
-            .on("select", applyCurrentSlideNum)
-    }
-
-
-
-
-
-
-
-    
-    function setSolutionsTabviewCarousel() {
-        var solutionsTabviewSliderNode = document.getElementById('solutions_tabview_slider')
-        var options = { loop: false, draggable: false }
-        var solutionsTabviewCarousel = EmblaCarousel(solutionsTabviewSliderNode, options)
-
-        for (let i = 0; i < tabsList.length; i++) {
-            tabsList[i].addEventListener("click", () => {
-                for (let j = 0; j < tabsList.length; j++) {
-                    tabsList[j].classList.remove("active");
-                }
-                tabsList[i].classList.add("active");
-                solutionsTabviewCarousel.scrollTo(i, true)
-            })
+        return {
+            applyProgress,
+            removeProgress,
         }
     }
 
-    setSolutionsTabviewCarousel();
-    setupHomeCarousel();
-})
+    const OPTIONS = { loop: true }
+    const homeSliderNode = document.getElementById('home_sldier');
+    const progressNode = document.getElementById('home_sider_progress_bar');
+    const homeSliderPrevBtn = document.getElementById("home_slider_prev_btn");
+    const homeSliderNextBtn = document.getElementById("home_slider_next_btn");
+    const homeSliderCurSlideNum = document.getElementById("home_slider_cur_slide_num");
+
+    const homeSliderCarousel = EmblaCarousel(homeSliderNode, OPTIONS)
+    const { applyProgress, removeProgress } = setupProgressBar(
+        homeSliderCarousel,
+        progressNode,
+    )
+
+    homeSliderNextBtn.onclick = homeSliderCarousel.scrollNext
+    homeSliderPrevBtn.onclick = homeSliderCarousel.scrollPrev
+
+    const applyCurrentSlideNum = () => {
+        const _value = homeSliderCarousel.selectedScrollSnap() + 1
+        if (_value < 10) homeSliderCurSlideNum.innerText = "0" + _value
+        else homeSliderCurSlideNum.innerText = _value
+
+    }
+
+    homeSliderCarousel
+        .on('init', () => {
+            applyProgress();
+            applyCurrentSlideNum();
+        })
+        .on('reInit', applyProgress)
+        .on('scroll', applyProgress)
+        .on('destroy', removeProgress)
+        .on("select", applyCurrentSlideNum)
+}
 
 
 
 
+
+
+
+
+function setSolutionsTabviewCarousel() {
+    var solutionsTabviewSliderNode = document.getElementById('solutions_tabview_slider')
+    var options = { loop: false, draggable: false }
+    var solutionsTabviewCarousel = EmblaCarousel(solutionsTabviewSliderNode, options)
+
+    for (let i = 0; i < tabsList.length; i++) {
+        tabsList[i].addEventListener("click", () => {
+            for (let j = 0; j < tabsList.length; j++) {
+                tabsList[j].classList.remove("active");
+            }
+            tabsList[i].classList.add("active");
+            solutionsTabviewCarousel.scrollTo(i, true)
+        })
+    }
+}
+
+setSolutionsTabviewCarousel();
+setupHomeCarousel();
 
 
 
